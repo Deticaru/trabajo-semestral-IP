6e76013189875d37ddb4264ad6113eb78b256516
Deticaru <125316596+Deticaru@users.noreply.github.com>
2025-05-14 08:20:29 -0400

    first commit

---
+++ backend/api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def hello(request):
+    return Response({"message:": "Hola desde Djando API"})
+
+
+
+# Create your views here.
