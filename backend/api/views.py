5385fa8f4e28fccca7de54ff4011f09c80e090ab
Deticaru <125316596+Deticaru@users.noreply.github.com>
2025-05-14 09:15:39 -0400

    inicio oficial de proyecto

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def hello(request):
-    return Response({"message:": "Hola desde Djando API"})
+    return Response({"message": "Hola desde Djando API"})
 
 
 
