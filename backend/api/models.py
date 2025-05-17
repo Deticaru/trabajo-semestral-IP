9755c57dc93a2b8e2c43ae22cb91c5d7bf02e910
deticaru <debasdfhk@gmail.com>
2025-05-17 13:10:55 -0400

    agregados a .gitignore y un poco de models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
-# Create your models here.
-class Producto(models.Model):
-    nombre_prod = models.CharField(max_length=100)
-    precio_prod = models.DecimalField(max_digits=10, decimal_places=2)
+class Marca(models.Model):
+    nom_marca   = models.CharField(max_length=50, verbose_name='Nombre Marca')
 
     def __str__(self):
-        return self.nombre_prod
+        return self.nom_marca
+    
+# Create your models here.
+class Producto(models.Model):
+    nom_prod    = models.CharField(max_length=100, verbose_name='Nombre Producto')
+    desc_prod   = models.CharField(max_length=1500, verbose_name='Descripción Producto')
+    precio_prod = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Precio Producto')
+    marca_prod  = models.ForeignKey(Marca, on_delete=models.CASCADE, verbose_name='Marca Producto')
+
+
+    def __str__(self):
+        return self.nom_prod
+    
