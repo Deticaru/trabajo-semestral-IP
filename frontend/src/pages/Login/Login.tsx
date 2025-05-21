f56039371234cd3c1382121dd0fa16b68ac0ff58
deticaru <debasdfhk@gmail.com>
2025-05-21 00:04:35 -0400

    BOtón de volver a la página de inicio

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -131,8 +131,28 @@ const Login: React.FC = () => {
         {/* INICIAR SESIÓN */}
         {isLoggingIn && ( 
           
-          <div className={`grid grid-cols-2 gap-3 form-container2 ${isLoggingIn ? (loginVisible ? 'fade-in' : 'fade-out') : 'hidden'}`}>
+          <div className={`grid grid-cols-2 gap-3 form-container2 relative ${isLoggingIn ? (loginVisible ? 'fade-in' : 'fade-out') : 'hidden'}`}>
             <div className="col-span-1">
+              <div className="grid grid-cols-3">
+                <div className="col-span-1">
+                  <button
+                    className="bg-white text-center w-35 rounded-2xl h-12 relative text-white text-xl font-semibold group"
+                    type="button"
+                    onClick={() => {
+                      navigate("/Home");
+                    }}
+                  >
+                    <div className="bg-red-800 rounded-xl h-10 w-1/4 flex items-center justify-center absolute left-1 top-[3px] group-hover:w-[130px] z-10 duration-600">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="25px" width="25px">
+                        <path d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z" fill="#000000" />
+                        <path d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z" fill="#000000" />
+                      </svg>
+                    </div>
+                    <p className="translate-x-2">Volver</p>
+                  </button>
+                </div>
+              </div>
+              <br />
               <p className="title">Iniciar Sesión</p>
 
               {/* FORM */}
