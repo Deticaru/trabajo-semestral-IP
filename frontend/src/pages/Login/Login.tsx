eff4e3167deb8b3026f0c8bb1b3c8e72e8e0efaf
deticaru <debasdfhk@gmail.com>
2025-05-20 22:25:15 -0400

    commit de branch

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -7,11 +7,20 @@ import LoginBackground from "../../assets/images/login-background.jpg"
 import "./Login.css"
 
 const Login: React.FC = () => {
+  const [isLoggingIn, setisLoggingIn] = useState(true);
+
   const [correo_usuario, setCorreo] = useState<string>("");
+  const [nombre_usuario, setNombre] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [telefono, setTelefono] = useState<string>("");
   const [error, setError] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true); 
   const navigate = useNavigate();
+  const [loginVisible, setLoginVisible] = useState(true);
+  const [registerVisible, setRegisterVisible] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  
+  
   // el error aquí es para quitar un mensaje de alerta de vscode, pero si es necesario la const 'error'
   error;
 
@@ -26,7 +35,6 @@ const Login: React.FC = () => {
       root.classList.add("login-root");
     }
 
-    // Set body background when this component is mounted
     document.body.style.backgroundImage = `url(${LoginBackground})`;
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundPosition = "center";
@@ -34,23 +42,58 @@ const Login: React.FC = () => {
 
     const token = localStorage.getItem("access_token");
     if (token) {
-      // Opcional: podrías verificar si el token es válido con el backend
       navigate("/Home");
       document.title = "Inicio";
     } else {
-      setLoading(false); // solo mostramos login si no está logueado
+      setLoading(false);
     }
 
     return () => {
       document.body.classList.remove("login-page");
       if (root) root.classList.remove("login-root");
-      // Reset when component unmounts (important!)
       document.body.style.backgroundImage = "";
       document.body.style.backgroundSize = "";
       document.body.style.backgroundPosition = "";
       document.body.style.backgroundRepeat = "";
     };
   }, []);
+
+  const Logging = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    if (isLoggingIn === true) {
+      alert("Está iniciando Sesión");
+    } else {
+      alert("Se está registrando");
+    }
+  };
+
+  const handleRegisterClick = () => {
+    setLoginVisible(false);
+    setTimeout(() => {
+      setisLoggingIn(false);
+    }, 500);
+    setTimeout(() => {
+      setRegisterVisible(true);
+    }, 600);
+  };
+
+  const handleBackToLogin = () => {
+    setRegisterVisible(false);
+    setTimeout(() => {
+      setisLoggingIn(true);
+    }, 500);
+    setTimeout(() => {
+      setLoginVisible(true);
+    }, 600);
+  };
+  
+  const handleTelefono = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const numberRegex = /^[0-9]*$/;
+    if (numberRegex.test(value)) {
+      setInputValue(value);
+    }
+  };
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -86,47 +129,88 @@ const Login: React.FC = () => {
     
     <div>
       <StyledWrapper>
-        <div className="grid grid-cols-2 gap-3 form-container2">
-          <div className="col-span-1">
-            <p className="title">Iniciar Sesión</p>
+        {/* INICIAR SESIÓN */}
+        {isLoggingIn && ( 
+          
+          <div className={`grid grid-cols-2 gap-3 form-container2 ${isLoggingIn ? (loginVisible ? 'fade-in' : 'fade-out') : 'hidden'}`}>
+            <div className="col-span-1">
+              <p className="title">Iniciar Sesión</p>
 
-            {/* FORM */}
-            <form className="form" onSubmit={handleLogin}>
-              <div className="input-group">
-                <label htmlFor="username">Correo</label>
-                <input type="email" name="username" id="username" placeholder="Ingresa tu correo..." value={correo_usuario} onChange={(e) => setCorreo(e.target.value)} required/>
-              </div>
-              <div className="input-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-                <div className="forgot">
-                  <a rel="noopener noreferrer" href="#">¿Olvidaste tu contraseña?</a>
+              {/* FORM */}
+              <form className="form" onSubmit={handleLogin}>
+                {/* CORREO */}
+                <div className="input-group">
+                  <label htmlFor="username">Correo</label>
+                  <input type="email" name="username" id="username" placeholder="Ingresa tu correo..." value={correo_usuario} onChange={(e) => setCorreo(e.target.value)} required/>
                 </div>
-              </div>
-              <button type="submit" className="sign">Iniciar Sesión</button>
-            </form>
-            {/* FORM */}
+                {/* CORREO FIN */}
+                {/* CONTRASEÑA */}
+                <div className="input-group">
+                  <label htmlFor="password">Password</label>
+                  <input type="password" name="password" id="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                  <div className="forgot">
+                    <a onClick={Logging}  rel="noopener noreferrer" href="#">¿Olvidaste tu contraseña?</a>
+                  </div>
+                </div>
+                {/* CONTRASEÑA FIN */}
+                <button type="submit" className="sign">Iniciar Sesión</button>
+              </form>
+              {/* FORM */}
+              <br />
+              <p className="signup">¿No tienes una cuenta?
+                <a onClick={handleRegisterClick} rel="noopener noreferrer" href="#" > Registrarse</a>
+              </p>
+            </div>
+            <div className="col-span-1" >
+              <img src={LogoLogin} alt="" />
+            </div>
+          </div>
+        )}
+        {/* FIN INICIAR SESIÓN */}
 
-            <div className="social-message">
-              <div className="line" />
-              <p className="message">Acceso rápido</p>
-              <div className="line" />
+        {/* REGISTRARSE */}
+        {!isLoggingIn && (
+          <div className={`grid grid-cols-2 gap-3 form-container3 ${!isLoggingIn ? (registerVisible ? 'fade-in' : 'fade-out') : 'hidden-initial'}`}>
+            
+            <div className="col-span-2">
+              <p className="title">Registración</p>
+
+              {/* FORM */}
+              <form className="form" onSubmit={handleLogin}>
+                {/* NOMBRE */}
+                <div className="input-group">
+                  <label htmlFor="nombrecom">Nombre Completo</label>
+                  <input type="text" name="nombrecom" id="nombrecom" placeholder="Ingresa tu nombre completo..." value={nombre_usuario} onChange={(e) => setNombre(e.target.value)} required/>
+                </div>
+                {/* NOMBRE FIN */}
+                {/* CORREO */}
+                <div className="input-group">
+                  <label htmlFor="username">Correo</label>
+                  <input type="email" name="username" id="username" placeholder="Ingresa tu correo..." value={correo_usuario} onChange={(e) => setCorreo(e.target.value)} required/>
+                </div>
+                {/* CORREO FIN */}
+                {/* CONTRASEÑA */}
+                <div className="input-group">
+                  <label htmlFor="password">Password</label>
+                  <input type="password" name="password" id="password" placeholder="Ingresa su contraseña..." value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                </div>
+                {/* CONTRASEÑA FIN */}
+                {/* TELÉFONO */}
+                <div className="input-group">
+                  <label htmlFor="telefono" className="telefono" >Teléfono</label>
+                  <input type="text" className="no-arrows" name="telefono" id="telefono" placeholder="Ingresa tu número telefónico..." value={inputValue} onChange={handleTelefono} required/>
+                </div>
+                <button type="submit" className="sign">Registrarse</button>
+              </form>
+              {/* FORM FIN */}
+              <br />
+              <p className="signup">¿Ya tienes una cuenta?
+                <a onClick={handleBackToLogin} href="#"> Iniciar Sesión</a>
+              </p>
             </div>
-            <div className="social-icons">
-              <button aria-label="Log in with Google" className="icon">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
-                  <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z" />
-                </svg>
-              </button>
-            </div>
-            <p className="signup">¿No tienes una cuenta?
-              <a rel="noopener noreferrer" href="#" > Registrarse</a>
-            </p>
           </div>
-          <div className="col-span-1" >
-            <img src={LogoLogin} alt="" />
-          </div>
-        </div>
+        )}
+        {/* FIN REGISTRARSE */}
       </StyledWrapper>
     </div>
   );
@@ -134,9 +218,26 @@ const Login: React.FC = () => {
 };
 const StyledWrapper = styled.div`
 
+  .fade-in {
+    opacity: 1;
+    transition: opacity 0.6s ease-in;
+  }
+
+  .fade-out {
+    opacity: 0;
+    transition: opacity 0.6s ease-out;
+  }
 
   .form-container2 {
     width: 1040px;
+    border-radius: 0.75rem;
+    background-color: rgba(17, 24, 39, 1);
+    padding: 2rem;
+    color: rgba(243, 244, 246, 1);
+  }
+  
+  .form-container3 {
+    width: 540px;
     border-radius: 0.75rem;
     background-color: rgba(17, 24, 39, 1);
     padding: 2rem;
