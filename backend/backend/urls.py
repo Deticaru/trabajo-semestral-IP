a3a25f5b126c2df271d90a40d10257377885f224
FelipeCSDav <nitruxbite@gmail.com>
2025-05-22 01:44:08 -0400

    Api Documentada con swagger

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,11 +24,28 @@ from rest_framework_simplejwt.views import (
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Documentación",
+      default_version='v1',
+      description="Documentación de la API del proyecto",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 # Agrega esto para servir archivos media (imágenes) en desarrollo
